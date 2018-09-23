80ef85ecb4d9dc62da5cdd0577e6dfd74c9ca2f0
Eze Gavilan <ezegavilan95@gmail.com>
2018-09-22 23:48:45 -0300

    Agregado al main del desafio01.

---
+++ src/main/java/clases/Pruebas.java
@@ -0,0 +1,18 @@
+package clases;
+
+import clases.desafio.Vector;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Pruebas {
+    public static void main(String[]args) throws IOException{
+
+        Vector v1 = new Vector();
+        v1.fillVector();
+       //Si haces esto rip cpu
+        //System.out.println(v1);
+    }
+
+
+}
